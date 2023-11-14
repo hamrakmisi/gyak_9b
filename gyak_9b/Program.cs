69f18521419599fca5929d5e31f48b0779f0a418
hamrakmisi <misi@hamy.hu>
2023-11-14 09:28:03 +0100

    Add project files.

---
+++ gyak_9b/Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseDefaultFiles();       //Ha nincs semmi a domain tuán, akkor az index.html-t tölti
+app.MapControllers();        //Az API Controllereket elérhetõvé teszi
+app.UseStaticFiles();
+
+app.Run();
